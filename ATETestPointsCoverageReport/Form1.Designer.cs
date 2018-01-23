119ad0885f9d31af3395295f50850b76cc306a8e
Edward <edward_song@yeah.net>
2018-01-23 17:26:52 +0800

    board ok

--- ATETestPointsCoverageReport/Form1.Designer.cs
+++ ATETestPointsCoverageReport/Form1.Designer.cs
@@ -28,12 +28,107 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.btnOpenBoard = new System.Windows.Forms.Button();
+            this.txtBoardxy = new System.Windows.Forms.TextBox();
+            this.txtBoard = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnRun);
+            this.groupBox1.Controls.Add(this.btnOpenBoard);
+            this.groupBox1.Controls.Add(this.txtBoardxy);
+            this.groupBox1.Controls.Add(this.txtBoard);
+            this.groupBox1.Location = new System.Drawing.Point(14, 26);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(512, 92);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 14);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "board_xy";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 14);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "board";
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(418, 27);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 50);
+            this.btnRun.TabIndex = 3;
+            this.btnRun.Text = "RUN";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // btnOpenBoard
+            // 
+            this.btnOpenBoard.Location = new System.Drawing.Point(337, 27);
+            this.btnOpenBoard.Name = "btnOpenBoard";
+            this.btnOpenBoard.Size = new System.Drawing.Size(75, 50);
+            this.btnOpenBoard.TabIndex = 2;
+            this.btnOpenBoard.Text = "OPEN";
+            this.btnOpenBoard.UseVisualStyleBackColor = true;
+            this.btnOpenBoard.Click += new System.EventHandler(this.btnOpenBoard_Click);
+            // 
+            // txtBoardxy
+            // 
+            this.txtBoardxy.Location = new System.Drawing.Point(75, 55);
+            this.txtBoardxy.Name = "txtBoardxy";
+            this.txtBoardxy.Size = new System.Drawing.Size(256, 22);
+            this.txtBoardxy.TabIndex = 1;
+            // 
+            // txtBoard
+            // 
+            this.txtBoard.Location = new System.Drawing.Point(75, 27);
+            this.txtBoard.Name = "txtBoard";
+            this.txtBoard.Size = new System.Drawing.Size(256, 22);
+            this.txtBoard.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 138);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.Button btnOpenBoard;
+        private System.Windows.Forms.TextBox txtBoardxy;
+        private System.Windows.Forms.TextBox txtBoard;
     }
 }
 
--- ATETestPointsCoverageReport/Form1.cs
+++ ATETestPointsCoverageReport/Form1.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using PlmBomToTxtBom;
+using System.IO;
 
 namespace ATETestPointsCoverageReport
 {
@@ -14,5 +16,183 @@ namespace ATETestPointsCoverageReport
         {
             InitializeComponent();
         }
+
+
+
+        public enum NodeProperty
+        {
+            NO_STATUS,
+            NO_PROBE,
+            MANDATORY,
+            NO_ACCESS,
+            PREFERRED
+           
+        }
+
+        public class Node
+        {
+            public string NodeName { set; get; }
+            public List<string> ComponentPin { set; get; }
+            public NodeProperty NodeProperty { set; get; }
+
+            public bool  IsTestPoint()
+            {
+                if (NodeProperty == Form1.NodeProperty.MANDATORY || NodeProperty == Form1.NodeProperty.PREFERRED)
+                    return true;
+                else
+                    return false;                    
+            }
+        }
+
+
+        public List<Node> allNode = new List<Node>();
+
+        private void btnOpenBoard_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Multiselect = true;
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                if (open.FileNames.Length >= 2)
+                {
+                    txtBoard.Text = open.FileNames[0];
+                    txtBoardxy.Text = open.FileNames[1];
+                }
+                else
+                    txtBoard.Text = open.FileName;
+            }
+        }
+
+
+
+
+
+
+        public List<string> getNodelist(string boardfile)
+        {
+
+            bool _isstartnode = false;
+            bool _isnode =false;
+            List<Node> listnode = new List<Node>();
+
+            List<string> nodelist = new List<string>();
+
+            string line = string.Empty;
+            StreamReader sr = new StreamReader(boardfile);
+            while (!sr.EndOfStream )
+            {
+                line = sr.ReadLine();
+                //MessageBox.Show(line);
+                if (line.ToUpper().Trim() == "CONNECTIONS")
+                    _isstartnode = true;
+                if (line .ToUpper ().Trim ()=="DEVICES")
+                    _isstartnode = false ;
+
+                if (_isstartnode && !line.Contains (".") && !string.IsNullOrEmpty (line))
+                {
+                    if (line.EndsWith(";"))
+                       line= line.Remove(line.Trim ().Length);
+                    nodelist.Add(line);
+                }
+
+
+
+            }
+
+
+
+            sr.Close();
+            return nodelist;
+        }
+
+
+        public List<Node> getNodelist1(string boardfile)
+        {
+
+            bool _isstartnode = false;
+            bool _isnode = false;
+            List<Node> listnode = new List<Node>();
+
+            List<string> nodelist = new List<string>();
+
+            string line = string.Empty;
+            StreamReader sr = new StreamReader(boardfile);
+            Dictionary<int, Node> nee = new Dictionary<int, Node>();
+            Node tempnode = new Node();
+            while (!sr.EndOfStream)
+            {
+                line = sr.ReadLine();
+                //MessageBox.Show(line);         
+
+              
+
+                if (_isstartnode )
+                {
+                    if (line.EndsWith(";"))
+                        line = line.Remove(line.Trim().Length);
+
+                    if (!string.IsNullOrEmpty(tempnode.NodeName) && !line.Contains("."))
+                    {
+                        listnode.Add(tempnode);
+                        tempnode = new Node();
+                    }
+
+                    if (!string.IsNullOrEmpty(line))
+                    {
+
+                        if (!line.Contains("."))
+                        {
+                            tempnode = new Node();
+                            tempnode.ComponentPin = new List<string>();
+                            tempnode.NodeName = line;
+                        }
+                        else
+                            tempnode.ComponentPin.Add(line.Trim());
+                    }
+
+                
+                }
+
+                if (line.ToUpper().Trim() == "CONNECTIONS")
+                    _isstartnode = true;
+                if ( line.ToUpper().Trim() == "DEVICES")
+                    _isstartnode = false;
+                if (line.ToUpper().Trim() == "NODES")
+                    _isstartnode = false;
+               
+
+
+            }
+
+
+
+            sr.Close();
+            return listnode;
+        }
+
+
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+          // List<string> nodelist = getNodelist(txtBoard.Text.Trim());
+           // nodelist.Sort();
+
+            allNode = getNodelist1(txtBoard.Text.Trim());
+
+           // MessageBox.Show(allNode[1721].NodeName);
+
+
+            StreamWriter sw = new StreamWriter(Application.StartupPath + @"\11.txt", true);
+
+            foreach (var item in allNode)
+            {
+                sw.WriteLine(item.NodeName);
+            }
+
+            sw.Close();
+
+            MessageBox.Show("OK");
+        }
+
     }
 }
